1335472b4816ffe7f5ef5319b305d9630ace083a
mush1997 <mush0104@gmail.com>
2025-11-20 12:21:01 +0800

    --debug--

--- src/components/BasicLayout.vue
+++ src/components/BasicLayout.vue
@@ -10,14 +10,15 @@ const ShoppingCart = defineAsyncComponent(() => import('@/components/ShoppingCar
 </script>
 
 <template>
-    <HeaderSection v-show="$route.name !== 'Home' && $route.name !== 'Category'" />
-    <router-view v-slot="{ Component, route }">
+    <!-- <HeaderSection v-show="$route.name !== 'Home' && $route.name !== 'Category'" /> -->
+    <!-- <router-view v-slot="{ Component, route }">
         <Transition :name="route.meta.transition || 'slide'" mode="out-in">
             <component :is="Component" :key="route.path" />
         </Transition>
-    </router-view>
-    <MenuList v-show="$route.name !== 'Home' && $route.name !== 'Checkout'" />
-    <AsideSection v-show="$route.name !== 'Checkout'" />
+    </router-view> -->
+    <p>123456789</p>
+    <!-- <MenuList v-show="$route.name !== 'Home' && $route.name !== 'Checkout'" /> -->
+    <!-- <AsideSection v-show="$route.name !== 'Checkout'" /> -->
     <FooterSection v-once />
     <BgShadow v-once />
     <ShoppingCart />
